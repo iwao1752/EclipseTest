88042734d0c936007be06682eacc494f06f35d91
iwao1752 <101.4.job@gmail.com>
2015-10-15 16:01:08 +0900

    update for loop statement

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -3,5 +3,9 @@ public class ArrayTest {
 	public static void main(String args[]){
 	    int[] arr = {1,2,3,4,5,6,7,8,9};
 	    System.out.println( arr.length );
+	    
+	    for( int i : arr){
+	    	System.out.println( i );
+	    }
 	}
 }
