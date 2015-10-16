f072ccdb3ae6fa7edb07eb84c6ed6098d8e2e61a
iwao1752 <101.4.job@gmail.com>
2015-10-16 18:30:25 +0900

    add method "ArgCalc"

--- src/ArgTest.java
+++ src/ArgTest.java
@@ -8,5 +8,12 @@ public class ArgTest {
 	public static void PrintName(String name){
 		System.out.println( "Hello!," + name );
 	}
+	
+	public static void ArgCalc(int x, int y, int z){
+		System.out.println("x = " + x + " / y = " + y + " / z = " + z );
+		System.out.println("------------------");
+		System.out.println(" add(x+y+z) result is... " + (x + y + z));
+		System.out.println(" div(x-y-z) result is... " + (x - y - z));
+	}
 
 }
